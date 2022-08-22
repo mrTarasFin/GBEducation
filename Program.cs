99150a93577bac6b0497da4e4d3adb498e11c9cf
Finic <finic@mail.ru>
2022-08-22 22:04:27 +0300

    For pushing

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,
+какое число большее, а какое меньшее.*/
+
+/*Console.WriteLine("Input first number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.WriteLine($"{num1} more than {num2}");
+}
+else
+{
+    Console.WriteLine($"{num2} more than {num1}");
+}*/
+
+
+/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.*/
+
+/*Console.WriteLine("Input first number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input third number:");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2 && num1 > num3)
+{
+    Console.WriteLine($"{num1} the biggest");
+}
+else if(num2 > num1 && num2 > num3)
+{
+    Console.WriteLine($"{num2} the biggest");
+}
+else
+{
+    Console.WriteLine($"{num3} the biggest");
+}
+*/
+
+
+/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт,
+является ли число чётным (делится ли оно на два без остатка).*/
+
+/*Console.WriteLine("Input number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if (num1%2 == 0)
+{
+    Console.WriteLine($"YES, number {num1} is even");
+}
+else
+{
+    Console.WriteLine($"NO, number {num1} is odd");
+}
+*/
+
+
+/*Задача 8: Напишите программу, которая на вход принимает число (N),
+а на выходе показывает все чётные числа от 1 до N.*/
+
+/*Console.WriteLine("Input number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+int current = 2;
+
+while( current <= num1)
+{
+    Console.WriteLine(current + " ");
+    current += 2;
+}
+*/
