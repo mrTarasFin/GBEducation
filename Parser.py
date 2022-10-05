e7f597f5dc12752d3746921c2c639d9b27937d12
tarasenkosv <sergey_tar@mail.ru>
2022-10-05 15:08:40 +0300

    Fix: commite

--- Parser.py
+++ Parser.py
@@ -1,9 +1,6 @@
 def coutn_len_str():
     str = ["sadf", "sdf", "sq", "e", "21234", "!@#"]
     new_str = []
-    for el in str:
-        if len(el) <= 3:
-            new_str.append(el)
-    print(new_str)
+
 
 coutn_len_str()
